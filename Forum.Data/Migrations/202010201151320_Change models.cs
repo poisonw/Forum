ca4543148164622d93db379a383c2b78563259bc
visualstudiome8@gmail.com <visualstudiome8@gmail.com>
2020-10-22 17:10:41 +0300

    Work on styles

---
+++ Forum.Data/Migrations/202010201151320_Change models.cs
@@ -0,0 +1,24 @@
+namespace Forum.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Changemodels : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Posts", name: "User_Id", newName: "UserId");
+            RenameColumn(table: "dbo.PostReplies", name: "User_Id", newName: "UserId");
+            RenameIndex(table: "dbo.Posts", name: "IX_User_Id", newName: "IX_UserId");
+            RenameIndex(table: "dbo.PostReplies", name: "IX_User_Id", newName: "IX_UserId");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.PostReplies", name: "IX_UserId", newName: "IX_User_Id");
+            RenameIndex(table: "dbo.Posts", name: "IX_UserId", newName: "IX_User_Id");
+            RenameColumn(table: "dbo.PostReplies", name: "UserId", newName: "User_Id");
+            RenameColumn(table: "dbo.Posts", name: "UserId", newName: "User_Id");
+        }
+    }
+}
--- Forum.Data/Models/ApplicationUser.cs
+++ Forum.Data/Models/ApplicationUser.cs
@@ -32,36 +32,7 @@ namespace Forum.Data.Models
         }
 
 
-        public class MyForum
-        {
-            public int Id { get; set; }
-            public string Title { get; set; }
-            public string Description { get; set; }
-            public DateTime Created { get; set; }
-            public string ImageUrl { get; set; }
-            public virtual ICollection<Post> Posts { get; set; }
-        }
-        public class Post
-        {
-            public int Id { get; set; }
-            public string Title { get; set; }
-            public string Content { get; set; }
-            public DateTime Created { get; set; }
 
-            public virtual ApplicationUser User { get; set; }
-            public virtual MyForum MyForum { get; set; }
-            public virtual ICollection<PostReply> Replies { get; set; }
-        }
-
-        public class PostReply
-        {
-        public int Id { get; set; }
-        public string Content { get; set; }
-        public DateTime Created { get; set; }
-
-        public virtual ApplicationUser User { get; set; }
-        public virtual Post Post { get; set; }
-    }
 }
 
 }
